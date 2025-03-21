fd3666b88e48db2fdf97f002cf80a1ba5fb7bd94
ItsMyDay797 <m.rus1298@gmail.com>
2025-03-21 23:40:41 +0300

    adding reown / view update

--- bicredit/AppDelegate.swift
+++ bicredit/AppDelegate.swift
@@ -6,6 +6,10 @@
 //
 
 import UIKit
+import WalletConnectNetworking
+import WalletConnectPairing
+import WalletConnectUtils
+
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,6 +18,16 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        let metadata = AppMetadata(
+            name: "BiCredit App",
+            description: "Decentralized lending app",
+            url: URL(string: "https://bicredit.app")!,
+            icons: [URL(string: "https://bicredit.app/icon.png")!]
+        )
+
+        let pairingClient = PairingClientFactory.create(metadata: metadata)
+
         return true
     }
 
@@ -31,6 +45,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
 
+    func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
+        pairingClient.handle(url: url)
+        return true
+    }
 
 }
 
--- bicredit/ContentView.swift
+++ bicredit/ContentView.swift
@@ -1,20 +1,7 @@
-import SwiftUI
-
 struct ContentView: View {
-    @State private var balance: String = "0"
-
     var body: some View {
-        VStack {
-            Text("Balance: \(balance) ETH")
-                .padding()
-            
-            Button("Get Balance") {
-                Task {
-                    let blockchainManager = BlockchainManager()
-                    balance = await blockchainManager.getBalance(address: "YOUR_WALLET_ADDRESS")
-                }
-            }
-            .padding()
+        NavigationView {
+            WalletConnectView()
         }
     }
 }
