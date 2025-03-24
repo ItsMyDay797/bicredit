e6cc7d8c324084a2fb0f88718d2d8ada13665a5d
ItsMyDay797 <m.rus1298@gmail.com>
2025-03-24 22:48:33 +0300

    Switch to wallet core

--- bicredit/ContentView.swift
+++ bicredit/ContentView.swift
@@ -1,7 +1,36 @@
+import SwiftUI
+import TrustWalletCore
+
 struct ContentView: View {
+    @State private var walletAddress: String = ""
+
     var body: some View {
-        NavigationView {
-            WalletConnectView()
+        VStack {
+            Text("Your Ethereum Wallet Address:")
+                .font(.headline)
+                .padding()
+
+            Text(walletAddress)
+                .font(.subheadline)
+                .padding()
+
+            Button(action: createWallet) {
+                Text("Generate New Wallet")
+                    .padding()
+                    .background(Color.blue)
+                    .foregroundColor(.white)
+                    .cornerRadius(10)
+            }
         }
+        .onAppear {
+            createWallet()
+        }
+    }
+
+    func createWallet() {
+        // Генерация новой seed-фразы и Ethereum-адреса
+        let wallet = HDWallet(strength: 128, passphrase: "")
+        let address = wallet.getAddressForCoin(coin: .ethereum)
+        walletAddress = address
     }
 }
--- bicredit/WalletManager.swift
+++ bicredit/WalletManager.swift
@@ -1,19 +1,1 @@
-import ReownAppKit
 
-class WalletManager {
-    private let client: WalletConnectClient
-
-    init() {
-        client = WalletConnectClient()
-    }
-
-    func connectWallet() {
-        client.pair { uri, error in
-            if let uri = uri {
-                print("Подключите кошелек, используя этот URI: \(uri)")
-            } else if let error = error {
-                print("Ошибка подключения: \(error.localizedDescription)")
-            }
-        }
-    }
-}
